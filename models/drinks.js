75f40b2046631ee3e5f1ad83678ae5b905198eaa
Victor Lopez <victorel94@gmail.com>
2019-09-30 18:29:13 -0700

    ignore

--- models/drinks.js
+++ models/drinks.js
@@ -1,44 +1,127 @@
 module.exports = function (sequelize, DataTypes) {
   var Drinks = sequelize.define("Drinks", {
     drink_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    spirit_one: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    spirit_two: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    spirit_three: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    spirit_four: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    spirit_five: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    mixer_one: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
-      }
+      },
+      defaultValue: 'none'
     },
-    spirits:
-    {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
+    mixer_two: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    mixer_three: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    mixer_four: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    mixer_five: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    syrup_one: {
+      type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
-      }
+      },
+      defaultValue: 'none'
     },
-    mixer: {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
-      allowNull: false
-    },
-    syrup: {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
+    syrup_two: {
+      type: DataTypes.STRING,
       allowNull: false,
+      defaultValue: 'none'
+    },
+    syrup_three: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    syrup_four: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+    syrup_five: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
     },
     ice: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    tag: {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
-      allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
     },
     garnish: {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
-      allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
     },
+    tag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'none'
+    },
+
     special_instructions: {
       type: DataTypes.STRING,
       allowNull: false,
+      defaultValue: 'none'
+    },
+    upRating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    downRating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
     }
   });
+
   return Drinks;
 };
