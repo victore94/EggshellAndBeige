f51bfa384b42bdea44663fa029908af35e6a75cd
unknown <h.e.ergaster@gmail.com>
2019-10-01 20:09:25 -0700

    testing

--- public/js/ageVar.js
+++ public/js/ageVar.js
@@ -4,13 +4,19 @@ var exit = document.getElementsByClassName("close")[0];
 window.onload = function() {
   modal.style.display = "block";
 
+  localStorage.getItem("is21")
+    ? (modal.style.display = "none")
+    : (modal.style.display = "block");
+
   exit.onclick = function() {
     location.href = "https://www.disney.com";
   };
   document.getElementById("no").onclick = function() {
+    localStorage.setItem("is21", false);
     location.href = "https://cponline.pw/";
   };
   document.getElementById("yes").onclick = function() {
+    localStorage.setItem("is21", true);
     modal.style.display = "none";
   };
 };
