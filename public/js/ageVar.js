ca8090f897c0aaf0af9c82d5578fc2ba1b513401
unknown <h.e.ergaster@gmail.com>
2019-10-01 19:12:53 -0700

    new angle on verification

--- public/js/ageVar.js
+++ public/js/ageVar.js
@@ -1,13 +1,16 @@
 var modal = document.getElementById("myModal");
-var totally = document.getElementsByClassName("close")[0];
+var exit = document.getElementsByClassName("close")[0];
 
 window.onload = function() {
   modal.style.display = "block";
 
-  totally.onclick = function() {
+  exit.onclick = function() {
     location.href = "https://www.disney.com";
   };
   document.getElementById("no").onclick = function() {
     location.href = "https://cponline.pw/";
+  };
+  document.getElementById("yes").onclick = function() {
+    modal.style.display = "none";
   };
 };
