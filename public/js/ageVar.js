e7c19f8bc3e2477adfd80958385bf15ddeaafe17
unknown <h.e.ergaster@gmail.com>
2019-10-01 17:04:29 -0700

    test modal

---
+++ public/js/ageVar.js
@@ -0,0 +1,10 @@
+// document.getElementById("no").onclick = function() {
+//   console.log("hello");
+//   location.href = "https://cponline.pw/";
+// };
+
+(function($) {
+  $.ageCheck = function(options) {
+    //our code goes here...
+  };
+});
