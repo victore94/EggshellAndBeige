8bc547bc14728682378387cc1af209b6f76d75fd
Victor Lopez <victorel94@gmail.com>
2019-10-03 20:25:52 -0700

    asdf

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,14 @@
 // this is where the AJAX is going
+
+$(document).ready(() => {
+    let $drinkInput = $('.spiritInput')
+
+    $(document).on('click', '.submitSpirit', insertDrinks)
+
+    function insertDrinks(e) {
+        let drink = {
+            spirit_one: $drinkInput.val().trim(''),
+        }
+        console.log(drink)
+    }
+})
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,8 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
 
-// Dependencies
-// =============================================================
 var path = require("path");
 
-// Routes
-// =============================================================
 module.exports = (app) => {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/cocktail.html"));
   });
